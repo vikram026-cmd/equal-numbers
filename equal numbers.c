cabe28f91d75b7436deeea2c6862aabe10a92394
THANGAVIKRAMAN R <thangavikraman55@gmail.com>
2024-09-30 22:26:04 +0530

    Add files via upload

---
+++ equal numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter a number a :");
+    scanf ("%d",&a);
+    printf("Enter a number b:");
+    scanf ("%d",&b);
+
+    if (a==b)
+    {
+        printf("The given number are equal");
+    }
+    else
+    {
+        printf("The given number is unequal");
+    }
+    return 0;
+    }
